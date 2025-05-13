de211136b17af5bff0d35c42ace8dbddf7fa2fd9
Willis Plummer <willisplummer@gmail.com>
2025-05-13 13:46:39 -0400

    format

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -34,55 +34,55 @@ return {
 			},
 		},
 		opts = {
-				-- Map of filetype to formatters
-				formatters_by_ft = {
-					lua = { "stylua" },
-					-- Conform will run multiple formatters sequentially
-					-- python = { "isort", "black" },
-					-- Use a sub-list to run only the first available formatter
-					javascript = { "prettierd", "prettier", stop_after_first = true },
-					typescript = { "prettierd", "prettier", stop_after_first = true },
-					javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-					nix = { "nixfmt", stop_after_first = true },
-					zig = { "zigfmt", stop_after_first = true },
-					-- Use the "*" filetype to run formatters on all filetypes.
-					-- ["*"] = { "codespell" },
-					-- Use the "_" filetype to run formatters on filetypes that don't
-					-- have other formatters configured.
-					["_"] = { "trim_whitespace" },
-				},
-				-- If this is set, Conform will run the formatter on save.
-				-- It will pass the table to conform.format().
-				-- This can also be a function that returns the table.
-				format_on_save = {
-					-- I recommend these options. See :help conform.format for details.
-					lsp_fallback = true,
-					timeout_ms = 500,
-				},
+			-- Map of filetype to formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				nix = { "nixfmt", stop_after_first = true },
+				zig = { "zigfmt", stop_after_first = true },
+				-- Use the "*" filetype to run formatters on all filetypes.
+				-- ["*"] = { "codespell" },
+				-- Use the "_" filetype to run formatters on filetypes that don't
+				-- have other formatters configured.
+				["_"] = { "trim_whitespace" },
+			},
+			-- If this is set, Conform will run the formatter on save.
+			-- It will pass the table to conform.format().
+			-- This can also be a function that returns the table.
+			format_on_save = {
+				-- I recommend these options. See :help conform.format for details.
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
 		},
 		config = function(_, opts)
 			local conform = require("conform")
 			conform.setup(opts)
 
-			vim.api.nvim_create_user_command('FormatDisable', function(args)
-    	  if args.bang then
-    	    -- :FormatDisable! disables autoformat for this buffer only
-    	    vim.b.disable_autoformat = true
-    	  else
-    	    -- :FormatDisable disables autoformat globally
-    	    vim.g.disable_autoformat = true
-    	  end
-    	end, {
-    	  desc = 'Disable autoformat-on-save',
-    	  bang = true, -- allows the ! variant
-    	})
-			vim.api.nvim_create_user_command('FormatEnable', function()
-  	    vim.b.disable_autoformat = false
-  	    vim.g.disable_autoformat = false
-  	  end, {
-			  desc = 'Re-enable autoformat-on-save',
-    	})
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- :FormatDisable! disables autoformat for this buffer only
+					vim.b.disable_autoformat = true
+				else
+					-- :FormatDisable disables autoformat globally
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true, -- allows the ! variant
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
 
 			vim.keymap.set("n", "<leader>f", function()
 				conform.format({ async = true })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,38 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "tsx",
-          "typescript",
-          "javascript",
-          "lua",
-          "json",
-          "html",
-          "css",
-          "markdown",
-          "markdown_inline",
-        },
-        highlight = {
-          enable = true, -- 🔧 turn this ON
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true, -- 🔧 also turn this ON
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            node_decremental = "grm",
-            scope_incremental = "grc",
-          },
-        },
-      })
-    end,
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"tsx",
+					"typescript",
+					"javascript",
+					"lua",
+					"json",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+				},
+				highlight = {
+					enable = true, -- 🔧 turn this ON
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true, -- 🔧 also turn this ON
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "gnn",
+						node_incremental = "grn",
+						node_decremental = "grm",
+						scope_incremental = "grc",
+					},
+				},
+			})
+		end,
+	},
 }
