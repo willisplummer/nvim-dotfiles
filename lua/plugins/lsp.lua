dea77e0877250e8bf6bfa4883c9d082c337c86d8
wplummer <wplummer@patreon.com>
2025-07-21 15:57:21 +0000

    add biome to lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,7 +13,7 @@ return {
 			require("mason").setup()
 			require("mason-lspconfig").setup({
 				-- NOTE: consider vtsls instead of ts_ls
-				ensure_installed = { "ts_ls", "eslint", "lua_ls" },
+				ensure_installed = { "ts_ls", "eslint", "lua_ls", "biome" },
 				automatic_installation = true,
 				automatic_enable = false,
 				handlers = {
@@ -139,6 +139,13 @@ return {
 				end,
 			})
 			lspconfig.lua_ls.setup({})
+			lspconfig.biome.setup({
+				root_dir = require("lspconfig.util").root_pattern("biome.json", ".git"),
+				workingDirectory = { mode = "location" }, -- safer than \"auto\" for plugin resolution
+				settings = {
+					nodePath = vim.fn.getcwd() .. "/node_modules",
+				},
+			})
 			lspconfig.eslint.setup({
 				root_dir = require("lspconfig.util").root_pattern(".eslintrc.js", "package.json"),
 				workingDirectory = { mode = "location" }, -- safer than \"auto\" for plugin resolution
