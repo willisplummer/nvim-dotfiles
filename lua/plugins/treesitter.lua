eee08d9d68d225210a5a40073c12ed2d8ff8cc74
wplummer <wplummer@patreon.com>
2026-02-18 16:16:59 +0000

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter").setup({
 				ensure_installed = {
 					"tsx",
 					"typescript",
